0e6af2b35048fb874c04eb29dc825f2364be1df8
Victor Tavares <victorlmt_1@hotmail.com>
2022-04-25 20:05:17 -0300

    API Pessoa

---
+++ Aula 01/Desafio01/Desafio01/Controllers/PessoaController.cs
@@ -0,0 +1,51 @@
+﻿using Desafio01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : ControllerBase
+    {      
+
+        private readonly ILogger<PessoaController> _logger;
+
+        public PessoaController(ILogger<PessoaController> logger)
+        {
+            Pessoa pessoa1 = new Pessoa("Arthur", "Vieira", 25, 4000.23m, "Técnico em redes");
+            Pessoa.AdicionarPessoa(pessoa1);
+            Pessoa pessoa2 = new Pessoa("João", "Silva", 30, 6028.00m, "Técnico em informática");
+            Pessoa.AdicionarPessoa(pessoa2);
+            Pessoa pessoa3 = new Pessoa("Maria", "Santos", 35, 5500.00m, "Desenvolvedor Júnior");
+            Pessoa.AdicionarPessoa(pessoa3);
+            Pessoa pessoa4 = new Pessoa("Pedro", "Álvares", 40, 15000.00m, "Capitão de fragata");
+            Pessoa.AdicionarPessoa(pessoa4);
+            Pessoa pessoa5 = new Pessoa("José", "Fernandes", 29, 8000.00m, "Pastor de ovelhas");
+            Pessoa.AdicionarPessoa(pessoa5);
+            _logger = logger;
+        }
+
+        [HttpGet("listarPessoas")]
+        public IEnumerable<Pessoa> Get()
+        {
+            return Pessoa.ListarPessoas();
+        }
+        
+        [HttpGet("listarPessoasAcimaDaMedia")]
+        public IEnumerable<Pessoa> GetMediaSalarial()
+        {
+            return Pessoa.ImprimirPessoasComMaiorSalarioMaiorQueAMedia();
+        }
+
+        [HttpGet("pessoaComMaiorSalario")]
+        public Pessoa GetPessoaComSalarioMaior()
+        {
+            return Pessoa.ImprimirPessoaComMaiorSalario();
+        }
+    }
+}
---
+++ Aula 01/Desafio01/Desafio01/Models/Pessoa.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Desafio01.Models
+{
+    public class Pessoa
+    {
+        private static readonly Pessoa[] _pessoas = new Pessoa[5];
+        public static Pessoa[] Pessoas = _pessoas;
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+        public decimal Salario { get; set; }
+        public string Profissao { get; set; }
+        public Pessoa(string nome, string sobrenome, int idade, decimal salario, string profissao)
+        {
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+            this.Idade = idade;
+            this.Salario = salario;
+            this.Profissao = profissao;
+        }
+        public static void AdicionarPessoa(Pessoa pessoa)
+        {
+            for (int i = 0; i < _pessoas.Length; i++)
+            {
+                if (_pessoas[i] == null)
+                {
+                    _pessoas[i] = pessoa;
+                    break;
+                }
+            }
+        }
+        public static void AdicionarPessoa(Pessoa[] array, Pessoa pessoa)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == null)
+                {
+                    array[i] = pessoa;
+                    break;
+                }
+            }
+        }
+        public static Pessoa[] ListarPessoas()
+        {
+            foreach (var pessoa in Pessoas)
+            {
+                if (pessoa != null)
+                {
+                    Debug.WriteLine($"Nome: {pessoa.Nome}");
+                    Debug.WriteLine($"Sobrenome: {pessoa.Sobrenome}");
+                    Debug.WriteLine($"Idade: {pessoa.Idade}");
+                    Debug.WriteLine($"Salário: {pessoa.Salario}");
+                    Debug.WriteLine($"Profissão: {pessoa.Profissao}");
+                }
+            }
+            return Pessoas;
+        }
+        public static decimal CalcularMediaSalarial()
+        {
+            decimal mediaSalarial = 0;
+            foreach (var pessoa in Pessoas)
+            {
+                if (pessoa != null)
+                {
+                    mediaSalarial += pessoa.Salario;
+                }
+            }
+            mediaSalarial = mediaSalarial / Pessoas.Length;
+            Debug.WriteLine($"Média salarial: {mediaSalarial}");
+            return mediaSalarial;
+        }
+        
+        public static Pessoa[] ImprimirPessoasComMaiorSalarioMaiorQueAMedia()
+        {
+            decimal mediaSalarial = CalcularMediaSalarial();
+            Pessoa[] pessoasComMaiorSalario = new Pessoa[5];
+            foreach (var pessoa in Pessoas)
+            {
+                if (pessoa != null)
+                {
+                    if (pessoa.Salario > mediaSalarial)
+                    {
+                        Debug.WriteLine($"Nome: {pessoa.Nome}");
+                        Debug.WriteLine($"Sobrenome: {pessoa.Sobrenome}");
+                        Debug.WriteLine($"Idade: {pessoa.Idade}");
+                        Debug.WriteLine($"Salário: {pessoa.Salario}");
+                        Debug.WriteLine($"Profissão: {pessoa.Profissao}");
+                        AdicionarPessoa(pessoasComMaiorSalario, pessoa);
+                    }
+                }
+            }
+            return pessoasComMaiorSalario;
+        }
+
+        public static Pessoa ImprimirPessoaComMaiorSalario()
+        {
+            Pessoa pessoaComMaiorSalario = null;
+            foreach(var pessoa in Pessoas)
+            {
+                if (pessoa != null)
+                {
+                    if (pessoaComMaiorSalario == null)
+                    {
+                        pessoaComMaiorSalario = pessoa;
+                    }
+                    else if (pessoa.Salario > pessoaComMaiorSalario.Salario)
+                    {
+                        pessoaComMaiorSalario = pessoa;
+                    }
+                }
+            }
+            return pessoaComMaiorSalario;
+        }
+    }
+}

535154464064370690bdd1d32bd2eb94b55d3c10
Victor Tavares <victor@v20.dev.br>
2022-05-02 20:02:59 -0300

    Aula sobre Pilhas

---
+++ Aula 01/Desafio01/Desafio01/Controllers/PilhaController.cs
@@ -0,0 +1,78 @@
+﻿using Desafio01.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PilhaController : ControllerBase
+    {
+        private static PilhaModel _pilha;
+        public PilhaController()
+        {
+
+        }
+        [HttpGet]
+        public IActionResult CriarPilha(int tamanho)
+        {
+            _pilha = new PilhaModel(tamanho);
+            return Ok($"Pilha criada com sucesso! Tamanho: {tamanho}");
+        }
+        [HttpPost("empilhar")]
+        public IActionResult Empilhar(char valor)
+        {
+            if (_pilha == null)
+            {
+                return BadRequest("Pilha não criada!");
+            }
+            if (_pilha.EstaCheia())
+            {
+                return BadRequest("Pilha cheia!");
+            }
+            _pilha.Empilhar(valor);
+            return Ok($"Valor {valor} inserido com sucesso!");
+        }
+
+        [HttpPost("desempilhar")]
+        public IActionResult Desempilhar()
+        {
+            if (_pilha == null)
+            {
+                return BadRequest("Pilha não criada!");
+            }
+            if (_pilha.EstaVazia())
+            {
+                return BadRequest("Pilha vazia!");
+            }
+            char valor = _pilha.Desempilhar();
+            return Ok($"Valor {valor} removido com sucesso!");
+        }
+        [HttpPost("consultar")]
+        public IActionResult Consultar()
+        {
+            if (_pilha == null)
+            {
+                return BadRequest("Pilha não criada!");
+            }
+            if (_pilha.EstaVazia())
+            {
+                return BadRequest("Pilha vazia!");
+            }
+            char valor = _pilha.Consultar();
+            return Ok($"Valor do topo: {valor}");
+        }
+
+        [HttpPost("avaliarExpressao")]
+        public IActionResult AvaliarExpressao(string expressao)
+        {
+            if (expressao == null || expressao.Length == 0)
+            {
+                return BadRequest("Expressão vazia!");
+            }
+            PilhaModel pilha = new PilhaModel(expressao.Length);
+            return Ok(pilha.AvaliarExpressao(expressao));
+            
+        }
+    }
+}
---
+++ Aula 01/Desafio01/Desafio01/Models/PilhaModel.cs
@@ -0,0 +1,79 @@
+﻿namespace Desafio01.Models
+{
+    public class PilhaModel
+    {
+        private int Topo { get; set; }
+        private char[] Pilha { get; set; }
+        public PilhaModel(int tamanho)
+        {
+            Pilha = new char[tamanho];
+        }
+        public void Empilhar(char valor)
+        {
+            if (Topo < Pilha.Length)
+            {
+                Pilha[Topo] = valor;
+                Topo++;
+            }
+        }
+        public char Desempilhar()
+        {
+            if (Topo > 0)
+            {
+                Topo--;
+                return Pilha[Topo];
+            }
+            throw new System.Exception("Pilha vazia!");
+        }
+        public char Consultar()
+        {
+            return Pilha[Topo - 1];
+        }
+        public bool EstaVazia()
+        {
+            return Topo == 0;
+        }
+        public bool EstaCheia()
+        {
+            return Topo == Pilha.Length;
+        }
+
+        public bool AvaliarExpressao(string expressao)
+        {
+            foreach (char c in expressao)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    Empilhar(c);
+                }
+                else if (c == ')' || c == '}' || c == ']')
+                {
+                    if (EstaVazia())
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        if (c == ')' && Consultar() == '(')
+                        {
+                            Desempilhar();
+                        }
+                        else if (c == '}' && Consultar() == '{')
+                        {
+                            Desempilhar();
+                        }
+                        else if (c == ']' && Consultar() == '[')
+                        {
+                            Desempilhar();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return EstaVazia();
+        }
+    }
+}
